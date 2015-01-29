7a9743aa7ee3fba5a2afda853b37891e69cdec4b
Michaelangelo1993 <h021876c@student.staffs.ac.uk>
2015-01-29 17:45:29 +0000

    BaseFramework

---
+++ Game/Game/AppMain.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using Sce.PlayStation.HighLevel.UI;
+	
+namespace Game
+{
+	public class AppMain
+	{
+		private static Sce.PlayStation.HighLevel.GameEngine2D.Scene 	gameScene;
+		private static Sce.PlayStation.HighLevel.UI.Scene 				uiScene;
+		
+		// Member Variables go here
+				
+		public static void Main (string[] args)
+		{
+			Initialize();
+			
+			//Game loop
+			bool quitGame = false;
+			while (!quitGame) 
+			{
+				Update ();
+				
+				Director.Instance.Update();
+				Director.Instance.Render();
+				UISystem.Render();
+				
+				Director.Instance.GL.Context.SwapBuffers();
+				Director.Instance.PostSwap();
+			}
+			
+			//Clean up after ourselves.
+			//Dispose code goes here
+			
+			Director.Terminate ();
+		}
+
+		public static void Initialize ()
+		{
+			//Set up director and UISystem.
+			Director.Initialize ();
+			UISystem.Initialize(Director.Instance.GL.Context);
+			
+			//Set game scene
+			gameScene = new Sce.PlayStation.HighLevel.GameEngine2D.Scene();
+			gameScene.Camera.SetViewFromViewport();
+			
+			//Set the ui scene.
+			uiScene = new Sce.PlayStation.HighLevel.UI.Scene();
+			Panel panel  = new Panel();
+			panel.Width  = Director.Instance.GL.Context.GetViewport().Width;
+			panel.Height = Director.Instance.GL.Context.GetViewport().Height;
+			
+			uiScene.RootWidget.AddChildLast(panel);
+			UISystem.SetScene(uiScene);
+			
+			//Run the scene.
+			Director.Instance.RunWithScene(gameScene, true);
+		}
+		
+		public static void Update()
+		{
+			//Determine whether the player tapped the screen
+			var touches = Touch.GetData(0);
+			
+			//If tapped, do something
+			if(touches.Count > 0)
+			{
+				
+			}
+				
+			// Move update code here
+			
+		}		
+	}
+}
